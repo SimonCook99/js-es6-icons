919ac3f6336f09bcc2a1c66b02b61068a4d68465
Simone Mauro <simon.mauro@virgilio.it>
2022-02-04 17:31:05 +0100

    Carte dinamiche (milestone 1 completato)

--- main.js
+++ main.js
@@ -112,3 +112,17 @@ let icons = [
 		color: 'blue'
 	}
 ];
+
+
+let container = document.getElementById("container");
+let content = "";
+for(let i = 0; i < icons.length; i++){
+    
+    content += `<div class="icon">
+                        <i style="color:${icons[i].color};" class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"></i>
+                        <div class="details">${icons[i].name}</div>
+                    </div>`;
+}
+
+
+container.innerHTML = content;
