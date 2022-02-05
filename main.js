5a88f2609e9fb1703ec0c0423c0f1539ed91ed18
Simone Mauro <simon.mauro@virgilio.it>
2022-02-05 17:23:30 +0100

    Filtro dinamico funzionante

--- main.js
+++ main.js
@@ -116,13 +116,56 @@ let icons = [
 
 let container = document.getElementById("container");
 let content = "";
+
+let selection = document.getElementById("filter");
+
+
+//primo ciclo per inserire dinamicamente tutti gli elementi
 for(let i = 0; i < icons.length; i++){
-    
-    content += `<div class="icon">
-                        <i style="color:${icons[i].color};" class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"></i>
-                        <div class="details">${icons[i].name}</div>
-                    </div>`;
+		
+	content += `<div class="icon">
+						<i style="color:${icons[i].color};" class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"></i>
+						<div class="details">${icons[i].name}</div>
+					</div>`;
+					
 }
 
-
 container.innerHTML = content;
+
+
+
+//evento che segna il cambio all'interno della selection
+selection.addEventListener("change", function(){
+	
+	//array filtrato in base alla corrispondenza tra il valore della select, e quello presente nell'array delle icone
+	let arrayFiltered = icons.filter(icon =>{
+
+		if(icon.type == this.value || this.value == ""){
+			return true;
+		}else{
+			return false;
+		}
+	});
+
+	console.log(arrayFiltered);
+
+	//svuoto il contenuto precedente prima di inserire quello nuovo
+	if(content != ""){
+		content = "";
+	}
+
+	//ciclo con l'array filtrato di elementi
+	for(let i = 0; i < arrayFiltered.length; i++){
+		
+		content += `<div class="icon">
+							<i style="color:${arrayFiltered[i].color};" class="${arrayFiltered[i].family} ${arrayFiltered[i].prefix}${arrayFiltered[i].name}"></i>
+							<div class="details">${arrayFiltered[i].name}</div>
+						</div>`;
+						
+	}
+
+	container.innerHTML = content;
+	
+});
+
+
